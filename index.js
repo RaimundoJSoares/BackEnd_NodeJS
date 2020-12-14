6d8d787c39789b43e608560d521604648f055dea
KazumaShachou <raianimes1000@gmail.com>
2020-12-14 13:50:44 -0400

    get,post

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-app.get('/mensagens', function (req, res) {
+app.use(bodyParser.json())
+
+const port = 3000;
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const mensagens = [
+  "Essa é a primeira mensagem",
+  "Essa é a segnda mensagem"
+];
+
+app.get('/mensagens',  (req, res) => {
   res.send('Exibir a lista de mensagens')
-})
+});
 
-app.post('/mensagens', function (req, res) {
-  res.send('Criar uma mensagens')
-})
+app.post('/mensagens', (req, res) => {
+  console.log(req.body);
+
+  const texto = req.body.texto;
+  
+  mensagens.push(texto);
+
+  res.send('Mensagem foi criada com sucesso')
+  res.send(mensagens);
+
+
+});
  
-app.listen(3000) 
+app.listen(port, () => {
+  console.info('App rodando em  http://localhost' + port);
+}) ;
